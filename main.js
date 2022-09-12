25d7d52bd197db478279bc8e84cdaf55f084adc4
Milind Waghmare <milind.waghmare@vistaura.com>
2022-09-12 09:42:37 +0530

    loading gltf with proper lights

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGL1Renderer();
 const camera = new THREE.PerspectiveCamera(75,  window.innerWidth/window.innerHeight, 0.1, 2000);
-
+// const raycaster = THREE.Raycaster()
 camera.position.set(5,5,5)
 renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio)
@@ -15,21 +15,22 @@ new OrbitControls(camera, renderer.domElement);
 
 //Light
 const ambientLightLight = new THREE.AmbientLight(0xffffff, 1)
-const PointLight = new THREE.PointLight(0xffffff, 7, 5, 1)
-PointLight.position.set(0,5,0)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1 )
+directionalLight.position.set(0,5,0)
 scene.add(ambientLightLight)
-scene.add(PointLight)
+scene.add(directionalLight)
 //Adding GLTF to scene
 const loader = new GLTFLoader();
 
 loader.load('/Formal_Shoe-White.glb', (glb) =>{
   console.log(glb.scene)
-  const children = [...glb.scene.children]
-  for (let child of children){
-
-    scene.add(child)
+ const sole = glb.scene.getObjectByName("Sole-Var1")
+  for(let mesh of glb.scene.children){
+    console.log(mesh.name);
   }
-  
+ 
+  scene.add(glb.scene)
+
 })
 
 
